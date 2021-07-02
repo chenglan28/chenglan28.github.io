9d085eddb956b45de37edf5072ad35e34a6f03ff
cdn2021 <78522762+cdn2021@users.noreply.github.com>
2021-07-02 21:28:31 +0800

    Create LoadPage.js

---
+++ js/LoadPage.js
@@ -0,0 +1,21 @@
+function LoadPage(url){
+  if (window.XMLHttpRequest){
+    var xhr = new XMLHttpRequest();
+  } else {
+    var xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  };
+  xhr.open("GET","/file/" + url,true);
+  xhr.send();
+  xhr.onreadystatechange = function(){
+    if (xhr.readystate == 4){
+      if (xhr.status >= 200 && xhr.status < 400){
+        document.body.innerHTML = xhr.response;
+      } else {
+        document.body.innerHTML = "Failed to load the page.<hr>AirServer Error Report<br>·HTTP STATUS " + xhr.status + "<br><br>·Please refresh the page.<br>·Please contact the owner of this website.<a href=mailto:>chenglan28</a> <a href=mailto:pcdn2021@outlook.com>cdn2021</a>";
+      };
+      if (!xhr.status){
+        document.body.innerHTML = "Failed to send the request.<hr>AirServer Error Report<br>·Please contact the owner of this website.<a href=mailto:>chenglan28</a> <a href=mailto:pcdn2021@outlook.com>cdn2021</a>";
+      };
+    };
+  };
+};
