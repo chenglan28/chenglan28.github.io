154cfb856cc566e78aa12af10f1e7b31f216d1c5
cdn2021 <78522762+cdn2021@users.noreply.github.com>
2021-07-03 12:57:12 +0800

    Update LoadPage.js

--- js/LoadPage.js
+++ js/LoadPage.js
@@ -10,30 +10,38 @@ function LoadPage(url){
     if (true || xhr.readystate == 4){
       if (xhr.status >= 200 && xhr.status < 400){
         document.body.innerHTML = xhr.response;
-        var requirescript = document.querySelector("#requirescript");
-        if (requirescript != null){
-          requirescript = JSON.parse(requirescript.getAttribute("data-source"));
-          var i = 0;
-          while (i < requirescript.length){
-            if (window.XMLHttpRequest){
-              var req = new XMLHttpRequest();
-            } else {
-              var req = new ActiveXObject("Microsoft.XMLHTTP");
+        try {
+          var requirescript = document.querySelector("#requirescript");
+          if (requirescript != null){
+            requirescript = JSON.parse(requirescript.getAttribute("data-source"));
+            var i = 0;
+            while (i < requirescript.length){
+              if (window.XMLHttpRequest){
+                var req = new XMLHttpRequest();
+              } else {
+                var req = new ActiveXObject("Microsoft.XMLHTTP");
+              };
+              console.log(requirescript[i]);
+              req.open("GET",requirescript[i],false);
+              req.send();
+              if (req.status >= 200 && req.status < 400)
+                eval(req.responseText);
+              else
+                console.error("Cannot load javascript file:" + requirescript[i]);
+              i++;
             };
-            console.log(requirescript[i]);
-            req.open("GET",requirescript[i],false);
-            req.send();
-            if (req.status >= 200 && req.status < 400)
-              eval(req.responseText);
-            else
-              console.error("Cannot load javascript file:" + requirescript[i]);
-            i++;
           };
+        } catch (err){
+          console.error(err);
         };
-        var parseScript = document.querySelector("#parsescript");
-        if (parseScript != null){
-          parseScript = parseScript.innerHTML;
-          eval(parseScript);
+        try {
+          var parseScript = document.querySelector("#parsescript");
+          if (parseScript != null){
+            parseScript = parseScript.innerHTML;
+            eval(parseScript);
+          };
+        } catch (err){
+          console.error(err);
         };
       } else {
         document.body.innerHTML = "Failed to load the page.<hr>AirServer Error Report<br>·HTTP STATUS " + xhr.status + "<br><br>·Please refresh the page.<br>·Please contact the owner of this website.<a href=mailto:chenglan_mc@163.com>chenglan28</a> <a href=mailto:pcdn2021@outlook.com>cdn2021</a>";
